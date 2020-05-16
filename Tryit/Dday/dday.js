a8cb93a8c7a638e3d47900ea526da90d12a29f79
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-05-16 20:59:18 +0900

    make dday caculator

--- Tryit/Dday/dday.js
+++ Tryit/Dday/dday.js
@@ -6,12 +6,11 @@ const firstDay = new Date("2018-09-15"),
     passedDay = Math.round(passedTime/(24*60*60*1000)),
     document.querySelector('#accent').innerHTML = passedDay + "일";
 
-
 function calcDate(days){
-    const future = toFisrt + days*(24,60,60,1000);
+    const future = toFisrt + days*(1000*60*60*24);
     const someday = new Date(future);
     const year = someday.getFullYear();
-    const month = someday.getMonth();
+    const month = someday.getMonth()+1;
     const date = someday.getDate();
     document.querySelector("#date"+days).innerHTML = year + "년" + month + "월" + date + "일" ;
 }
