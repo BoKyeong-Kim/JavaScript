cc651e744df364d8cfaf51b6c038c8fc23afbc79
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-06-30 08:45:00 +0900

    javascript make D-day

---
+++ Tryit/Dday/dday.js
@@ -0,0 +1,27 @@
+const firstDay = new Date("2018-09-15"),
+    nowDay = new Date(),
+    toFisrt = firstDay.getTime(),
+    toNow = nowDay.getTime(),
+    passedTime = toNow - toFisrt;
+    passedDay = Math.round(passedTime/(24*60*60*1000)),
+    document.querySelector('#accent').innerHTML = passedDay + "일";
+
+
+function calcDate(days){
+    const future = toFisrt + days*(24,60,60,1000);
+    const someday = new Date(future);
+    const year = someday.getFullYear();
+    const month = someday.getMonth();
+    const date = someday.getDate();
+    document.querySelector("#date"+days).innerHTML = year + "년" + month + "월" + date + "일" ;
+}
+
+function init() {
+    calcDate(100);
+    calcDate(200);
+    calcDate(365);
+    calcDate(500);
+    calcDate(1000);
+}
+
+init();
