fbaf3821daf656c786976e0a114236e255288a10
BoKyeong-Kim <bokyeong3659@gmail.com>
2020-10-07 22:08:54 +0900

    remove hard coding part - div class chat messages

--- Chat/server.js
+++ Chat/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 //socket.io를 사용하려면 직접 액세스해야함
@@ -11,24 +12,26 @@ const io = socketio(server);
 //정적 폴더 설정 -> 브라우저에서 HTTP를 열면(localhost 3000) index.html 페이지를 볼 수 있음.
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 //클라이언트 연결 실행
 io.on('connection', socket=> {
     //console.log('New Web Socket Connection...');
 
-    // welcome current user
-    socket.emit('message', 'Welcome to ChatCord!');
+    // welcome current user - 실제로 보내고 싶은 메세지 
+    socket.emit('message', formatMessage(botName,'Welcome to ChatCord!'));
 
     // 사용자가 연결할 때의 Broadcast
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName,'A user has joined the chat'));
 
     // 클라이언트 연결 해제 실행
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName,'A user has left the chat'));
     })
 
     // listen to chatMessage 
     socket.on('chatMessage', msg => {
-    io.emit('message', msg);
+    io.emit('message', formatMessage('USER',msg));
     })
 
 });
